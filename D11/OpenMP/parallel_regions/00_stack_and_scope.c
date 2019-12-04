1875fd90c5016e1c490dd4cd234405ac04392602
lucatornatore <luca.tornatore@inaf.it>
2019-12-04 16:21:22 +0100

    update

--- D11/OpenMP/parallel_regions/00_stack_and_scope.c
+++ D11/OpenMP/parallel_regions/00_stack_and_scope.c
@@ -31,6 +31,9 @@
 #  endif
 #endif
 #define _GNU_SOURCE
+#ifndef _OPENMP
+#error "openmp support is required to compile this code"
+#endif
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -41,8 +44,8 @@
 
 int main( int argc, char **argv )
 {
-  int    i;
-
+  int i;
+  int nthreads=1; 
   register unsigned long long base_of_stack asm("rbp");
   register unsigned long long top_of_stack asm("rsp");
 
@@ -59,13 +62,26 @@ int main( int argc, char **argv )
 	  &i,
 	  (void*)base_of_stack - (void*)&i,
 	  (void*)&i - (void*)top_of_stack );
+
+#pragma omp parallel
+#pragma omp master
+  nthreads = omp_get_num_threads();
+
+  printf("using %d threads\n", nthreads);
+  size_t stack_base_addresses[ nthreads ];
+  size_t stack_top_addresses[ nthreads ];
   
-  // just try who is the private i for each thread
+  // also prove who is the private i for each thread
 #pragma omp parallel private(i)
   {
     int me = omp_get_thread_num();
     unsigned long long my_stackbase;
+    unsigned long long my_stacktop;
     __asm__("mov %%rbp,%0" : "=mr" (my_stackbase));
+    __asm__("mov %%rsp,%0" : "=mr" (my_stacktop));
+
+    stack_base_addresses[me] = my_stackbase;
+    stack_top_addresses[me] = my_stacktop;
     
     printf( "thread (tid: %ld) nr %d:\n"
 	    "\tmy base of stack is %p ( %td from main\'s stack )\n"
@@ -76,6 +92,30 @@ int main( int argc, char **argv )
 	    &i, (void*)&i - (void*)my_stackbase, (void*)&i - (void*)base_of_stack);	    
   }
 
-  printf( "\n" );  
+  printf( "\n" );
+
+  for( i = 0; i < nthreads; i++ )
+    {
+      printf("thread %d: bp @ %p, tp @ %p: %lld B", i,
+	     (void*)stack_base_addresses[i],
+	     (void*)stack_top_addresses[i],
+	     (long long int)(stack_base_addresses[i]-stack_top_addresses[i]));
+      if (i > 0 )
+	printf("\t --> %lld B from top of thread %d",
+	       (long long int)(stack_base_addresses[i]-stack_top_addresses[i-1]), i-1);
+      printf("\n");
+    }
+
+  printf("\n");
+
+  char *omp_stacksize = NULL;
+
+  if ( (omp_stacksize = getenv("OMP_STACKSIZE")) == NULL )
+    printf("you did not define OMP_STACKSIZE: try to set it and check what happens to the threads' stack pointers\n");
+  else
+    printf("you defined OMP_STACKSIZE as %s: try to change that value and check what happens to the threads' stack pointers\n",
+	   omp_stacksize);
+
+  printf("\n");
   return 0;
 }
